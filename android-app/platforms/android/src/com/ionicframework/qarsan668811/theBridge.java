2731edf50dbdba18ca0f6d37ec57c96b9fbe0e1e
Michael Weingert <mpweinge@uwaterloo.ca>
2015-04-11 15:59:31 +0400

    Stubbing in Firebase

--- android-app/platforms/android/src/com/ionicframework/qarsan668811/theBridge.java
+++ android-app/platforms/android/src/com/ionicframework/qarsan668811/theBridge.java
@@ -3,6 +3,7 @@ package com.ionicframework.qarsan668811;
 import android.os.Bundle;
 import org.apache.cordova.*;
 import java.util.*;
+import com.firebase.client.*;
 
 public class theBridge
 {
@@ -12,6 +13,19 @@ public class theBridge
 
     protected theBridge() {
         // Exists only to defeat instantiation.
+        Firebase.setAndroidContext(this);
+        Firebase myFirebaseRef = new Firebase("https://brilliant-torch-1595.firebaseio.com/");
+        myFirebaseRef.child("TEST").setValue("HI THERE");
+
+        /* THIS IS HOW TO READ
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+  @Override
+  public void onDataChange(DataSnapshot snapshot) {
+    System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+  }
+  @Override public void onCancelled(FirebaseError error) { }
+});
+         */
     }
     public static theBridge getInstance() {
         if(singleton == null) {
